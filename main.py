6aa4b65696cac8182006afdf8a574c485612a63e
Derrick Auyoung <derrick.auyoung@gmail.com>
2025-12-01 09:12:55 -0800

    added manual add_cors_headers method

--- main.py
+++ main.py
@@ -47,6 +47,13 @@ CORS(
     methods=["GET", "POST", "OPTIONS"]
 )
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "https://derrickauyoung.github.io"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"]
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"]
+    return response
+
 @app.route('/', methods=['GET', 'OPTIONS'])
 def index():
     """Serve the main index.html file."""
